134486d1535adacbb44c980440c4d168aff4a7ee
Adelamor <vl.vl.vl.vl.vl.vl@mail.ru>
2020-12-25 10:02:51 +0500

    init

---
+++ Assets/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D Rigidbody2D;
+    [SerializeField] private int m_Speed = 10;
+
+    private Vector2 m_StartSpeed;
+    
+    private void Start()    
+    {
+        Rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        m_StartSpeed = new Vector2(m_Speed,m_Speed);
+        Rigidbody2D.velocity = m_StartSpeed;
+    }
+    private void FixedUpdate()
+    {
+        var currentVelocity = Rigidbody2D.velocity;
+        currentVelocity = currentVelocity.normalized * m_StartSpeed.magnitude;
+        Rigidbody2D.velocity = currentVelocity;
+    }
+}
---
+++ Assets/Scitps/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D Rigidbody2D;
+    [SerializeField] private int m_JumpPower = 10;
+
+    private Vector2 m_StartSpeed;
+    
+    private void Start()    
+    {
+        m_StartSpeed = new Vector2(m_JumpPower,m_JumpPower);
+        Rigidbody2D.velocity =m_StartSpeed;
+    }
+
+    private void Update()
+    {
+    }
+    private void FixedUpdate()
+    {
+        var currentVelocity = Rigidbody2D.velocity;
+        currentVelocity = currentVelocity.normalized * m_StartSpeed.magnitude;
+        Rigidbody2D.velocity = currentVelocity;
+    }
+}
