48f2e3dedcfb317a9adc91dcb52a9aedb5ac8a8f
Adelamor <vl.vl.vl.vl.vl.vl@mail.ru>
2020-12-28 10:29:15 +0500

    Lesson 8

--- Assets/Scitps/App.cs
+++ Assets/Scitps/App.cs
@@ -1,55 +1,111 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class App : MonoBehaviour
 {
-    public TextMeshProUGUI ScoreText;
+    public enum ScreenName
+    {
+        Menu,
+        Game,
+        Lose,
+        Win
+    }
+
+    [Serializable]
+    public class UIScreen
+    {
+        public ScreenName Name;
+        public GameObject Screen;
+    }
+    
+    public TextMeshProUGUI[] ScoreTexts;
     public GameObject GamePrefab;
+
+    [Space]
+    public int ScoresToWin = 1;
+    public UIScreen[] Screens;
 
     private GameObject m_Game;
     private Ball m_Ball;
     private int m_Scores;
 
-    void Start()
+    private void Start()
     {
         Application.targetFrameRate = 60;
-        RestartGame();
-        UpdateScores();
+        SwitchUI(ScreenName.Menu);
     }
-    private void Update() 
-    {
-        if(Input.GetKeyDown(KeyCode.R))
-        {
-            RestartGame();
-        }
-    }
-    public void RestartGame()
+
+    public void StartGame()
     {
         m_Scores = 0;
         UpdateScores();
+
         if(m_Game !=null)
         {
             Destroy(m_Game);
         }
+
         m_Game = Instantiate(GamePrefab, Vector3.zero, Quaternion.identity);
         m_Ball = FindObjectOfType<Ball>();
 
         m_Ball.OnTouchUpper += () => ChangeScores(1); 
         m_Ball.OnTouchBottom += () => ChangeScores(-1);
+        SwitchUI(ScreenName.Game);        
     }
+    public void GoToMenu()
+    {
+        if(m_Game !=null) Destroy(m_Game);
+        SwitchUI(ScreenName.Menu);
+    }
+
+    public void GameLose()
+    {
+        if(m_Game !=null) Destroy(m_Game);   
+        SwitchUI(ScreenName.Lose);     
+    }
+
+    public void GameWin()
+    {
+        if(m_Game !=null) Destroy(m_Game);
+        SwitchUI(ScreenName.Win);
+    }
+
+    private void SwitchUI(ScreenName screenName)
+    {
+        for (int i = 0; i < Screens.Length; i++)
+        {
+            var item = Screens[i];
+            item.Screen.SetActive(item.Name == screenName);            
+        }
+    }
+
     private void ChangeScores (int value)
     {
         m_Scores += value;
-        if (m_Scores <= 0) m_Scores = 0;
+        if (m_Scores <= 0) 
+        {
+            m_Scores = 0;
+            GameLose();
+        }
+        
+        else if(m_Scores >= ScoresToWin)
+        {
+            GameWin();
+        }
         UpdateScores();
     }
 
 
     private void UpdateScores()
     {
-        ScoreText.text = m_Scores.ToString();
+        foreach (var item in ScoreTexts)
+        {
+            item.text = m_Scores.ToString();
+        }
+        
     }
 
 }
